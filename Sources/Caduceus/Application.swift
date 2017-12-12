24ffaf6439424e0d5bb299c8199dbaf29182c6a6
Chris Daley <chebizarro@gmail.com>
2017-12-12 02:19:19 -0800

    added class stubs

---
+++ Sources/Caduceus/Application.swift
@@ -0,0 +1,39 @@
+//===----------------------------------------------------------------------===//
+// Application.swift
+// 
+// This source file is part of the Cyllene open source project
+// https://github.com/cyllene-project
+// 
+// Copyright (c) 2017 Chris Daley <chebizarro@gmail.com>
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// 
+// See http://www.apache.org/licenses/LICENSE-2.0 for license information
+
+//===----------------------------------------------------------------------===//
+
+public class Application {
+	
+	public var configuration: Configuration
+	
+	public var display: Display
+	
+	public var name: String { get set }
+	
+	public var title: String { get set }
+	
+	init() {
+		
+	}
+	
+	public func start() {
+		
+	}
+	
+	public func quit() {
+		
+	}
+	
+	
+}
